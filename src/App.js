b3616a707498b6caabdf7edc445a84484e3035e0
ridz0326 <79255865+ridz0326@users.noreply.github.com>
2021-07-26 14:32:51 +0530

    CSS updated

--- src/App.js
+++ src/App.js
@@ -5,35 +5,35 @@ import Sudokuboxes from './Sudokuboxes'
 
 
 function App() {
-  var sudokuarray = [8, 2, 7, 1, 5, 4, 3, 9, 6,
-    9, 6, 5, 3, 2, 7, 1, 4, 8,
-    3, 4, 1, 6, 8, 9, 7, 5, 2,
-    5, 9, 3, 4, 6, 8, 2, 7, 1,
-    4, 7, 2, 5, 1, 3, 6, 8, 9,
-    6, 1, 8, 9, 7, 2, 4, 3, 5,
-    7, 8, 6, 2, 3, 5, 9, 1, 4,
-    1, 5, 4, 7, 9, 6, 8, 2, 3,
-    2, 3, 9, 8, 4, 1, 5, 6, 7]
+  var sudokuarray =[8, 2, 7, 1, 5, 4, 3, 9, 6,
+                    9, 6, 5, 3, 2, 7, 1, 4, 8,
+                    3, 4, 1, 6, 8, 9, 7, 5, 2,
+                    5, 9, 3, 4, 6, 8, 2, 7, 1,
+                    4, 7, 2, 5, 1, 3, 6, 8, 9,
+                    6, 1, 8, 9, 7, 2, 4, 3, 5,
+                    7, 8, 6, 2, 3, 5, 9, 1, 4,
+                    1, 5, 4, 7, 9, 6, 8, 2, 3,
+                    2, 3, 9, 8, 4, 1, 5, 6, 7]
 
   var gayabArray = [0, 0, 7, 0, 0, 4, 0, 9, 0,
-    9, 6, 0, 3, 2, 0, 1, 4, 8,
-    0, 0, 1, 6, 0, 9, 0, 0, 2,
-    5, 9, 3, 4, 0, 8, 0, 0, 1,
-    0, 0, 2, 5, 1, 3, 6, 0, 9,
-    6, 1, 0, 9, 0, 2, 0, 0, 5,
-    7, 0, 6, 2, 0, 5, 9, 0, 0,
-    0, 0, 4, 0, 9, 0, 8, 2, 3,
-    2, 3, 9, 0, 4, 1, 5, 0, 7]
+                    9, 6, 0, 3, 2, 0, 1, 4, 8,
+                    0, 0, 1, 6, 0, 9, 0, 0, 2,
+                    5, 9, 3, 4, 0, 8, 0, 0, 1,
+                    0, 0, 2, 5, 1, 3, 6, 0, 9,
+                    6, 1, 0, 9, 0, 2, 0, 0, 5,
+                    7, 0, 6, 2, 0, 5, 9, 0, 0,
+                    0, 0, 4, 0, 9, 0, 8, 2, 3,
+                    2, 3, 9, 0, 4, 1, 5, 0, 7]
 
   return (
     <>
       <h1>Sudoku</h1>
 
       <div className="btn">
-        <h2>Time:&nbsp;
+        <h3>Time:&nbsp;
         <span id="time" className="time">0</span>
         sec
-        </h2>
+        </h3>
       </div>
 
       <div className="App">
@@ -43,8 +43,20 @@ function App() {
 
             (val === gayabArray[index]) ?
               <Sudokuboxes key={index} index={index} sudokuarray={sudokuarray} /> :
-              <Input type="number" key={index} className="inputsselect2" defaultValue={val} ></Input>
+              <Input type="number" key={index} className="inputsselect2" value={val} ></Input>
           )}
+      
+      <div class="sudoku">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+      </div>
 
       </div>
 
@@ -52,7 +64,6 @@ function App() {
 
     </>
   )
-
 
 }
 
